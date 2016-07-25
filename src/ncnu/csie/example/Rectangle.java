2628ee62312b9fa4896673a7713309183b2809a5
jcliu <jcliu@ncnu.edu.tw>
2016-07-25 10:52:48 +0800

    Add files via upload

---
+++ src/ncnu/csie/example/Rectangle.java
@@ -0,0 +1,10 @@
+package ncnu.csie.example;
+
+public class Rectangle {
+	int w;
+	int h;
+	public int area(){
+		return w * h ;
+	}
+
+}
---
+++ src/ncnu/csie/example/helloworld.java
@@ -0,0 +1,14 @@
+package ncnu.csie.example;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle r1=new Rectangle();
+		r1.w=5;
+		r1.h=5;
+		System.out.println("r1ªº­±¿n: "+r1.area());
+
+	}
+
+}
